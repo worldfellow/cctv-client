38d32b1293b99c52c3ee02ab20de13c9ab729795
jeeshansufi <jeeshan@edulab.in>
2026-04-04 16:49:00 +0530

    Device Setting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { authGuard } from './guards/auth.guard';
+import { adminGuard } from './guards/admin.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -19,7 +20,8 @@ export const routes: Routes = [
             { path: 'add-camera', loadComponent: () => import('./components/camera-registration/camera-registration.component').then(m => m.CameraRegistrationComponent) },
             { path: 'access-control', loadComponent: () => import('./components/access-control/access-control.component').then(m => m.AccessControlComponent) },
             { path: 'screenshots', loadComponent: () => import('./components/screenshot-gallery/screenshot-gallery.component').then(m => m.ScreenshotGalleryComponent) },
-            { path: 'branding-settings', loadComponent: () => import('./components/branding-settings/branding-settings.component').then(m => m.BrandingSettingsComponent) }
+            { path: 'branding-settings', canActivate: [adminGuard], loadComponent: () => import('./components/branding-settings/branding-settings.component').then(m => m.BrandingSettingsComponent) },
+            { path: 'devicesetup', canActivate: [adminGuard], loadComponent: () => import('./components/device-setup/device-setup.component').then(m => m.DeviceSetupComponent) }
         ]
     },
     { path: '**', redirectTo: 'login' }
